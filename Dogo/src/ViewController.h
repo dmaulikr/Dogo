5d1dde81e9fecbce790c809ae325889410aa5a81
Marsal Silveira <marsal.silveira@gmail.com>
2015-09-04 17:28:21 -0300

    Initial commit

---
+++ Dogo/src/ViewController.h
@@ -0,0 +1,11 @@
+//
+//  ViewController.h
+//  Dogo
+//
+//  Created by Marsal on 04/09/15.
+//  Copyright (c) 2015 Marsal Silveira. All rights reserved.
+//
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'Dogo' do
+
+pod 'AFNetworking'
+
+end
+
+target 'DogoTests' do
+
+end
